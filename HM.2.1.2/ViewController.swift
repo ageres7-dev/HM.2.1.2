6992c063ef20ffbd23d86a34760f7e5af1dc0865
Sergey Dolgikh <ageres7@gmail.com>
2020-10-14 00:34:04 +0500

    Interface ready

--- HM.2.1.2/ViewController.swift
+++ HM.2.1.2/ViewController.swift
@@ -9,11 +9,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var redLightsView: UIView!
+    @IBOutlet var yellowLightsView: UIView!
+    @IBOutlet var greenLightsView: UIView!
+    
+    @IBOutlet var startButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        redLightsView.layer.cornerRadius = 50
+        yellowLightsView.layer.cornerRadius = 50
+        greenLightsView.layer.cornerRadius = 50
+        
+        redLightsView.alpha = 0.3
+        yellowLightsView.alpha = 0.3
+        greenLightsView.alpha = 0.3
+        
+        startButton.layer.cornerRadius = 10
+        
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func startButtonPressed() {
+        startButton.setTitle("NEXT", for: .normal)
+        
+        redLightsView.alpha = 1
+        
+    }
+    
 }
 
